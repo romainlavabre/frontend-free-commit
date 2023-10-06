e849cc8f9650a832578ba491466176941de1b262
romainlavabre <romainlavabre98@gmail.com>
2023-10-06 14:12:05 +0200

    FIX build

--- src/component/project/build/SeeLog.js
+++ src/component/project/build/SeeLog.js
@@ -1,11 +1,13 @@
-import {useParams} from "react-router";
+import {useNavigate, useParams} from "react-router";
 import React, {useEffect, useRef, useState} from "react";
 import api from "../../../api/api";
 import isNull from "../../../mixin/global/isNull";
 import {useDispatch} from "react-redux";
 import {openAlert} from "../../../store/util";
+import BackIcon from "../../util/icon/BackIcon";
 
 export default function SeeLog() {
+    const navigate = useNavigate();
     const {buildId, id} = useParams();
     const dispatch = useDispatch();
     const [logs, setLogs] = useState('');
@@ -66,38 +68,45 @@ export default function SeeLog() {
 
     return (
         <>
-            <div className="bg-light p-10 mt-4">
-                <h4 className="text-center text-fairfair text-3xl my-5">Logs</h4>
-
-                {
-                    isDynamic
-                        ? (
-                            <button disabled
-                                    className="text-blue-500 animate-spin">
-                                <svg xmlns=" http://www.w3.org/2000/svg"
-                                     className="h-10 w-10" fill="none"
-                                     viewBox="0 0 24 24"
-                                     stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                                          d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"/>
-                                </svg>
-                            </button>
-                        )
-                        : 'Build complete'
-                }
-                <div
-                    className="border-2 border-fairfair px-3 overflow-y-scroll overflow-x-scroll text-xl shadow-inner shadow-color-white bg-console"
-                    style={{maxHeight: '600px', maxWidth: '1550px'}}>
-
-                    <div className="log break-words" dangerouslySetInnerHTML={{
-                        __html: logs.split('\n').join('<br/>')
-                    }}>
-
-                    </div>
-
+            <div className="flex justify-between">
+                <div>
+                    <h4 className="text-3xl">Logs</h4>
+                </div>
+                <div>
+                    <button className="badge-blue-square" onClick={() => navigate("/project")}>
+                        <BackIcon size={8}/>
+                    </button>
                 </div>
             </div>
+
+            <hr className="mx-auto w-8/12 my-5"/>
+            {
+                isDynamic
+                    ? (
+                        <button disabled
+                                className="text-blue-500 animate-spin">
+                            <svg xmlns=" http://www.w3.org/2000/svg"
+                                 className="h-10 w-10" fill="none"
+                                 viewBox="0 0 24 24"
+                                 stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                                      d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"/>
+                            </svg>
+                        </button>
+                    )
+                    : 'Build complete'
+            }
+            <div
+                className="border-2 border-fairfair px-3 overflow-y-scroll overflow-x-scroll text-xl shadow-inner shadow-color-white bg-console"
+                style={{maxHeight: '600px', maxWidth: '1550px'}}>
+
+                <div className="log break-words" dangerouslySetInnerHTML={{
+                    __html: logs.split('\n').join('<br/>')
+                }}>
+
+                </div>
+
+            </div>
         </>
-    )
-        ;
+    );
 }
