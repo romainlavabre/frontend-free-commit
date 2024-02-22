724d1d340dd7734371c041a7184a037fa9cf7846
romainlavabre <romainlavabre98@gmail.com>
2024-02-22 09:47:11 +0100

    [3.0.0] Refactor

--- build.sh
+++ build.sh
@@ -2,9 +2,6 @@
 
 docker build -t romainlavabre/free-commit-client:"$1" -t romainlavabre/free-commit-client:latest -f ./docker/live/Dockerfile .
 
-docker logout
-docker login
-
 docker push romainlavabre/free-commit-client:"$1"
 docker push romainlavabre/free-commit-client:latest
 
